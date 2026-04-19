c3c3d1a8406d4faaffdb8d3e54a74cda1b2a453e
MxrixLu <m.benavidesd@uniandes.edu.co>
2026-04-19 13:13:27 -0500

    UML DB

--- calico-db.session.sql
+++ calico-db.session.sql
@@ -1,1 +1,10 @@
-UPDATE "users" SET "is_tutor_approved" = true WHERE "email" = 'benabenabena@yopmail.com';
+SELECT
+  table_name,
+  ordinal_position AS col_order,
+  column_name,
+  data_type,
+  is_nullable,
+  column_default
+FROM information_schema.columns
+WHERE table_schema = 'public'
+ORDER BY table_name, ordinal_position;
